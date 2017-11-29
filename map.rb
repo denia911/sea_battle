1a7614525eb26285e97dc861f2a8c52023550151
Denis <denisosa911@gmail.com>
2017-11-29 12:50:07 +0200

    Edited map.rb

--- map.rb
+++ map.rb
@@ -6,11 +6,6 @@ class Map
     @battlemap = IO.read('battlemap.txt')
   end
 
-  def letter_index(letter)
-    letters = ('A'..'J').to_a
-    letters.index letter
-  end
-
   def beng(put_arr)
     coordinates(put_arr)
     unless (0..10).to_a.include?(@f_el && @s_el)
@@ -20,8 +15,15 @@ class Map
     map[@f_el][@s_el] = @battlemap[@f_el * 10 + @s_el + @f_el]
   end
 
+  private
+
   def coordinates(put_arr)
     @f_el = letter_index(put_arr[0].to_s.upcase)
     @s_el = put_arr[1].to_i - 1
+  end
+
+  def letter_index(letter)
+    letters = ('A'..'J').to_a
+    letters.index letter
   end
 end
