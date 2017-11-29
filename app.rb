c0639edada40b4ec836f8ded0db6775b0e4eaec6
Denis <denisosa911@gmail.com>
2017-11-29 12:01:33 +0200

    Fixed map.rb, app.rb, added battleship.txt.

--- app.rb
+++ app.rb
@@ -1,20 +1,15 @@
 require_relative 'map'
-require_relative 'battlemap'
 require_relative 'draw_map'
 
 arr = Map.new
 drawmap = DrawMap.new(arr.map)
 drawmap.draw_map
 
-battle_map = BattleMap.new
-
 put = nil
 until put == 'end'
   puts 'Put the coordinates in the form like  A.1'
   put = gets.chomp
   put_arr = put.split('.')
-  f_el = Map.new.letter_index(put_arr[0].to_s.upcase)
-  s_el = put_arr[1].to_i - 1
-  arr.map[f_el][s_el] = battle_map.map[f_el][s_el]
+  arr.beng(put_arr)
   drawmap.draw_map
 end
--- map.rb
+++ map.rb
@@ -3,10 +3,25 @@ class Map
   attr_reader :map
   def initialize
     @map = (1..10).map { Array.new(10) { |_| '.' } }
+    @battlemap = IO.read('battlemap.txt')
   end
 
   def letter_index(letter)
     letters = ('A'..'J').to_a
     letters.index letter
+  end
+
+  def beng(put_arr)
+    coordinates(put_arr)
+    unless (0..10).to_a.include?(@f_el && @s_el)
+      print "You entered incorrect coordinates\n"
+      return
+    end
+    map[@f_el][@s_el] = @battlemap[@f_el * 10 + @s_el + @f_el]
+  end
+
+  def coordinates(put_arr)
+    @f_el = letter_index(put_arr[0].to_s.upcase)
+    @s_el = put_arr[1].to_i - 1
   end
 end
